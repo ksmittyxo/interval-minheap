20f19b60e51989ecdab87b012aca3a0affffaed2
ksmittyxo <80164202+ksmittyxo@users.noreply.github.com>
2024-03-14 21:07:07 -0500

    Added tests.

--- src/MinHeapTest.java
+++ src/MinHeapTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDateTime;
@@ -7,7 +8,7 @@ import java.util.ArrayList;
 
 public class MinHeapTest {
     @Test
-    public void testInsertAndExtract() {
+    public void testHeapOrder() {
         MinHeap minHeap = new MinHeap();
         minHeap.insert(new Interval(
                 LocalDateTime.of(2024, Month.JANUARY, 1, 0, 0),
@@ -29,6 +30,8 @@ public class MinHeapTest {
         sortedIntervals.add(minHeap.extract());
         sortedIntervals.add(minHeap.extract());
         sortedIntervals.add(minHeap.extract());
+        sortedIntervals.add(minHeap.extract());
+        sortedIntervals.add(minHeap.extract());
 
         ArrayList<Interval> testList = new ArrayList<>();
         testList.add(new Interval(
@@ -40,8 +43,114 @@ public class MinHeapTest {
         testList.add(new Interval(
                 LocalDateTime.of(2024, Month.MARCH, 1, 0, 0),
                 LocalDateTime.of(2024, Month.MARCH, 25, 0, 0)));
+        testList.add(new Interval(
+                LocalDateTime.of(2024, Month.AUGUST, 20, 0, 0),
+                LocalDateTime.of(2024, Month.OCTOBER, 31, 0, 0)));
+        testList.add(new Interval(
+                LocalDateTime.of(2024, Month.OCTOBER, 31, 0, 0),
+                LocalDateTime.of(2024, Month.NOVEMBER, 20, 0, 0)));
 
         assertEquals(testList, sortedIntervals);
+    }
+
+    @Test
+    public void testHeapOrder_duplicates() {
+        MinHeap minHeap = new MinHeap();
+        minHeap.insert(new Interval(
+                LocalDateTime.of(2024, Month.JANUARY, 1, 0, 0),
+                LocalDateTime.of(2024, Month.JANUARY, 8, 0, 0)));
+        minHeap.insert(new Interval(
+                LocalDateTime.of(2024, Month.MARCH, 1, 0, 0),
+                LocalDateTime.of(2024, Month.MARCH, 25, 0, 0)));
+        minHeap.insert(new Interval(
+                LocalDateTime.of(2024, Month.JANUARY, 1, 0, 0),
+                LocalDateTime.of(2024, Month.FEBRUARY, 14, 0, 0)));
+        minHeap.insert(new Interval(
+                LocalDateTime.of(2024, Month.JANUARY, 1, 0, 0),
+                LocalDateTime.of(2024, Month.JANUARY, 8, 0, 0)));
+        minHeap.insert(new Interval(
+                LocalDateTime.of(2024, Month.AUGUST, 20, 0, 0),
+                LocalDateTime.of(2024, Month.OCTOBER, 31, 0, 0)));
+
+        ArrayList<Interval> sortedIntervals = new ArrayList<>();
+        sortedIntervals.add(minHeap.extract());
+        sortedIntervals.add(minHeap.extract());
+        sortedIntervals.add(minHeap.extract());
+        sortedIntervals.add(minHeap.extract());
+        sortedIntervals.add(minHeap.extract());
+
+        ArrayList<Interval> testList = new ArrayList<>();
+        testList.add(new Interval(
+                LocalDateTime.of(2024, Month.JANUARY, 1, 0, 0),
+                LocalDateTime.of(2024, Month.JANUARY, 8, 0, 0)));
+        testList.add(new Interval(
+                LocalDateTime.of(2024, Month.JANUARY, 1, 0, 0),
+                LocalDateTime.of(2024, Month.JANUARY, 8, 0, 0)));
+        testList.add(new Interval(
+                LocalDateTime.of(2024, Month.JANUARY, 1, 0, 0),
+                LocalDateTime.of(2024, Month.FEBRUARY, 14, 0, 0)));
+        testList.add(new Interval(
+                LocalDateTime.of(2024, Month.MARCH, 1, 0, 0),
+                LocalDateTime.of(2024, Month.MARCH, 25, 0, 0)));
+        testList.add(new Interval(
+                LocalDateTime.of(2024, Month.AUGUST, 20, 0, 0),
+                LocalDateTime.of(2024, Month.OCTOBER, 31, 0, 0)));
+
+        assertEquals(testList, sortedIntervals);
+    }
+
+    @Test
+    public void testHeapOrder_singleElement() {
+        MinHeap minHeap = new MinHeap();
+        minHeap.insert(new Interval(
+                LocalDateTime.of(2024, Month.JANUARY, 1, 0, 0),
+                LocalDateTime.of(2024, Month.JANUARY, 8, 0, 0)));
+
+        ArrayList<Interval> sortedIntervals = new ArrayList<>();
+        sortedIntervals.add(minHeap.extract());
+
+        ArrayList<Interval> testList = new ArrayList<>();
+        testList.add(new Interval(
+                LocalDateTime.of(2024, Month.JANUARY, 1, 0, 0),
+                LocalDateTime.of(2024, Month.JANUARY, 8, 0, 0)));
+
+        assertEquals(testList, sortedIntervals);
+    }
+
+    @Test
+    public void testOverlapping() {
+        MinHeap minHeap = new MinHeap();
+        Interval interval = minHeap.overlapping(
+                new Interval(
+                        LocalDateTime.of(2024, Month.JANUARY, 1, 0, 0),
+                        LocalDateTime.of(2024, Month.JANUARY, 8, 0, 0)),
+                new Interval(
+                        LocalDateTime.of(2024, Month.JANUARY, 1, 0, 0),
+                        LocalDateTime.of(2024, Month.FEBRUARY, 14, 0, 0)));
+
+        Interval testInterval = new Interval(
+                LocalDateTime.of(2024, Month.JANUARY, 1, 0, 0),
+                LocalDateTime.of(2024, Month.FEBRUARY, 14, 0, 0));
+
+        assertEquals(interval, testInterval);
+    }
+
+    @Test
+    public void testOverlapping_notOverlapping() {
+        MinHeap minHeap = new MinHeap();
+        Interval interval = minHeap.overlapping(
+                new Interval(
+                        LocalDateTime.of(2024, Month.JANUARY, 1, 0, 0),
+                        LocalDateTime.of(2024, Month.JANUARY, 8, 0, 0)),
+                new Interval(
+                        LocalDateTime.of(2024, Month.AUGUST, 20, 0, 0),
+                        LocalDateTime.of(2024, Month.OCTOBER, 31, 0, 0)));
+
+        Interval testInterval = new Interval(
+                LocalDateTime.of(0, Month.JANUARY, 1, 0, 0),
+                LocalDateTime.of(0, Month.JANUARY, 1, 0, 0));
+
+        assertEquals(interval, testInterval);
     }
 
 }
